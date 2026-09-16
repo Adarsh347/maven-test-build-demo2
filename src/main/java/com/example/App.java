d287306a0d6c4ce90e10806ed68577a4cf867fba
Adarsh Chandran kv <144888670+Adarsh347@users.noreply.github.com>
2026-09-16 12:00:31 +0530

    Add App class with message and main method

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static String getMessage() {
+        return "Hello from Jenkins + Maven + GitHub!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
